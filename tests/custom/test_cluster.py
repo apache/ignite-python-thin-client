caaa7ecc826b807b485cc8f7325864f96ed2b00e
Igor Sapego <isapego@apache.org>
2021-04-01 19:26:50 -0700

    IGNITE-14465: Improve tests

--- tests/custom/test_cluster.py
+++ tests/custom/test_cluster.py
@@ -15,30 +15,49 @@
 
 import pytest
 
+from pyignite.exceptions import CacheError
 from tests.util import kill_process_tree
 
 from pyignite.datatypes.cluster_state import ClusterState
 
 
 def test_cluster_set_active(start_ignite_server, start_client):
+    key = 42
+    val = 42
+
     server1 = start_ignite_server(idx=1)
     server2 = start_ignite_server(idx=2)
     try:
         client = start_client()
-
         with client.connect([("127.0.0.1", 10801), ("127.0.0.1", 10802)]):
             cluster = client.get_cluster()
-
             assert cluster.get_state() == ClusterState.ACTIVE
+
+            cache = client.get_or_create_cache("test_cache")
+            cache.put(key, val)
+            assert cache.get(key) == val
 
             cluster.set_state(ClusterState.ACTIVE_READ_ONLY)
             assert cluster.get_state() == ClusterState.ACTIVE_READ_ONLY
 
+            assert cache.get(key) == val
+            with pytest.raises(CacheError):
+                cache.put(key, val + 1)
+
             cluster.set_state(ClusterState.INACTIVE)
             assert cluster.get_state() == ClusterState.INACTIVE
 
+            with pytest.raises(CacheError):
+                cache.get(key)
+
+            with pytest.raises(CacheError):
+                cache.put(key, val + 1)
+
             cluster.set_state(ClusterState.ACTIVE)
             assert cluster.get_state() == ClusterState.ACTIVE
+
+            cache.put(key, val + 2)
+            assert cache.get(key) == val + 2
     finally:
         kill_process_tree(server1.pid)
         kill_process_tree(server2.pid)
@@ -46,23 +65,42 @@ def test_cluster_set_active(start_ignite_server, start_client):
 
 @pytest.mark.asyncio
 async def test_cluster_set_active_async(start_ignite_server, start_async_client):
+    key = 42
+    val = 42
+
     server1 = start_ignite_server(idx=1)
     server2 = start_ignite_server(idx=2)
     try:
         client = start_async_client()
         async with client.connect([("127.0.0.1", 10801), ("127.0.0.1", 10802)]):
             cluster = client.get_cluster()
-
             assert await cluster.get_state() == ClusterState.ACTIVE
+
+            cache = await client.get_or_create_cache("test_cache")
+            await cache.put(key, val)
+            assert await cache.get(key) == val
 
             await cluster.set_state(ClusterState.ACTIVE_READ_ONLY)
             assert await cluster.get_state() == ClusterState.ACTIVE_READ_ONLY
 
+            assert await cache.get(key) == val
+            with pytest.raises(CacheError):
+                await cache.put(key, val + 1)
+
             await cluster.set_state(ClusterState.INACTIVE)
             assert await cluster.get_state() == ClusterState.INACTIVE
 
+            with pytest.raises(CacheError):
+                await cache.get(key)
+
+            with pytest.raises(CacheError):
+                await cache.put(key, val + 1)
+
             await cluster.set_state(ClusterState.ACTIVE)
             assert await cluster.get_state() == ClusterState.ACTIVE
+
+            await cache.put(key, val + 2)
+            assert await cache.get(key) == val + 2
     finally:
         kill_process_tree(server1.pid)
         kill_process_tree(server2.pid)
