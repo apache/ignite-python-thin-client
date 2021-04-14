8c172ce5397c830ccfd609393a18aa79cdcce1f8
Igor Sapego <isapego@apache.org>
2021-04-14 14:03:51 +0300

    IGNITE-14534 Add script to build wheels on Windows

---
+++ scripts/BuildWheels.ps1
@@ -0,0 +1,18 @@
+$PyVers="36","37","38","39"
+
+[System.Collections.ArrayList]$PyVersFull = $PyVers
+foreach ($Ver in $PyVers)
+{
+	[Void]$PyVersFull.Add("$Ver-32")
+}
+
+foreach ($Ver in $PyVersFull)
+{
+	& "$env:LOCALAPPDATA\Programs\Python\Python$Ver\python.exe" -m venv epy$Ver
+	
+	. ".\epy$Ver\Scripts\Activate.ps1"
+	pip install -e .
+	pip install wheel
+	pip wheel . --no-deps
+}
+
