cef4f35d6da5cf44abf6b198dccada8788b9dc3c
Aleksandr Nikolaev <>
2026-06-10 15:39:41 +0300

    IGNITE-28761 fix gh actions

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ def run_setup(with_binary=True):
     setuptools.setup(
         name='pyignite',
         version=version,
-        python_requires='>=3.7',
+        python_requires='>=3.8',
         author='The Apache Software Foundation',
         author_email='dev@ignite.apache.org',
         description='Apache Ignite binary client Python API',
@@ -106,7 +106,6 @@ def run_setup(with_binary=True):
         classifiers=[
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
